95a45b79a4d1b791ecacae2c700fa5e803a35792
Sithdown <afdezmnez@gmail.com>
2023-05-30 13:07:18 +0200

    Update ProjectManager.py
    Added Themed Tkinter bootstrap

--- ProjectManager.py
+++ ProjectManager.py
@@ -1,34 +1,48 @@
 import os
+import sys
 import shutil
 import datetime
 import json
 import re
 import zipfile
 
-import tkinter as tk
+import ttkbootstrap as tk
+from ttkbootstrap.constants import *
 from tkinter import simpledialog
 
+path = os.path.realpath(
+            os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+logstdout = os.path.join(path, "Logs", "stdout-pm_pyw.txt")
+logstderr = os.path.join(path, "Logs", "stderr-pm_pyw.txt")
+
+sys.stdout = open(logstdout, "w")
+sys.stderr = open(logstderr, "w")
+
+
 def mainLoop():
-    ROOT = tk.Tk()
+    ROOT = tk.Window(themename="darkly")
 
     ROOT.eval('tk::PlaceWindow . center')
-    ROOT.title('Project Manager')
-    ROOT.geometry('270x100')
+    ROOT.title('SPManager')
+    ROOT.resizable(False, False)
+    ROOT.geometry('240x60')
+    ROOT.lift()
+    ROOT.call('wm', 'attributes', '.', '-topmost', True)
 
-    frame = tk.Frame(ROOT)
-    frame.pack(expand=True)
-
-    text_disp= tk.Button(frame, 
-                       text="New", 
+    text_disp= tk.Button(ROOT, 
+                       text="New",
+                       bootstyle="success",
                        command=createNewProject
                        )
 
-    text_disp.pack(side=tk.LEFT)
+    text_disp.pack(side=tk.LEFT, padx=5, pady=10)
 
-    exit_button = tk.Button(frame,
+    exit_button = tk.Button(ROOT,
                        text="Clean",
+                       bootstyle="danger-outline",
                        command=cleanProjects)
-    exit_button.pack(side=tk.RIGHT)
+    exit_button.pack(side=tk.RIGHT, padx=5, pady=10)
 
     ROOT.mainloop()
 
@@ -55,6 +69,10 @@ def createNewProject():
     now = datetime.datetime.now()
     year = '{:02d}'.format(now.year)
     month = '{:02d}'.format(now.month)
+    #day = '{:02d}'.format(now.day)
+    #hour = '{:02d}'.format(now.hour)
+    #minute = '{:02d}'.format(now.minute)
+    #day_month_year = '{}-{}-{}'.format(year, month, day)
     month_name = now.strftime("%B")
 
     createPath(root, year)
@@ -63,19 +81,21 @@ def createNewProject():
 
     createPath(yearPath, monthPath)
 
-    ROOT = tk.Tk()
+    ROOT = tk.Window(themename="darkly")
     ROOT.withdraw()
     # the input dialog
     newProjectName = simpledialog.askstring(title="Project Creator", prompt="Project Name?")
 
-    config['lastProjectNumber'] += 1;
+    config['lastProjectNumber'] = config['lastProjectNumber'] + 1;
 
     projectPath = os.path.join(monthPath, str(config['lastProjectNumber']).zfill(3)+"_"+newProjectName)
 
     for path in createPaths:
         createPath(projectPath,path)
 
+
     with open('config.json', "w") as configFile:
+        print(json.dumps(config, indent=4))
         configFile.write(json.dumps(config, indent=4))
 
     quit()
@@ -111,6 +131,7 @@ def rPaths(d):
             if(path not in ignorePaths):
                 rPaths(newPath)
             else:
+                #print("IGNORED "+newPath)
                 break
 
     dirName = os.path.basename(d)
@@ -162,7 +183,7 @@ def cleanProjects():
 
 
 #open json config file
-configFile = open('config.json')
+configFile = open(os.path.join(path,'config.json'))
 config = json.load(configFile)
 configFile.close()
 
